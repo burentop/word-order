49c57b284e82f444f37f0b1c5cda4232b5a7ed58
Brent Perry <burentoperii@gmail.com>
2016-12-15 14:34:00 -0600

    add functions to show all unique words and their count.

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,27 @@
+var toArray = function (text) {
+  return text.split(/[\s,.!?]+/);
+}
+
+var toMap = function (array) {
+  var map = {};
+  array.forEach(function (item) {
+    if (!map[item]) {
+      map[item] = 1;
+    } else {
+      map[item] += 1;
+    }
+  });
+  return map;
+}
+
 $(document).ready(function () {
   $(".text-input form").submit(function (event) {
     event.preventDefault();
     var inputText = $("#block").val();
+    var ulMap = toMap(toArray(inputText));
+    Object.keys(ulMap).forEach(function (key) {
+      $("#list").append("<li>" + key + ": " + ulMap[key] + "</li>");
+    });
+    $(".output").show();
   })
 })
